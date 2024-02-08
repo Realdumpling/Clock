bb12752369fdfc842820f5f9b4aab0b982145fb1
Ivan <gluhovivan13@gmail.com>
2024-02-08 15:40:41 +0500

    Добавьте файлы проекта.

---
+++ Clock/Clk.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Threading;
+
+namespace Clock
+{
+    /// <summary>
+    /// Логика взаимодействия для Clk.xaml
+    /// </summary>
+    public partial class Clk : Window
+    {
+        double screenHeight = SystemParameters.FullPrimaryScreenHeight;
+        double screenWidth = SystemParameters.FullPrimaryScreenWidth;
+
+        public Clk()
+        {
+
+            InitializeComponent();
+            this.Top = screenHeight / screenHeight;
+            this.Left = screenWidth / screenWidth;
+
+        }
+
+
+
+
+        private void txt_Initialized(object sender, EventArgs e)
+        {
+            var timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.IsEnabled = true;
+            timer.Tick += (o, t) => { txt.Text = DateTime.Now.ToLongTimeString(); };
+            timer.Start();
+        }
+
+
+
+
+
+        private void txt_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void ccp_Loaded(object sender, RoutedEventArgs e)
+        {
+            WindowInteropHelper wndHelper = new WindowInteropHelper(this);
+
+            int exStyle = (int)GetWindowLong(wndHelper.Handle, (int)GetWindowLongFields.GWL_EXSTYLE);
+
+            exStyle |= (int)ExtendedWindowStyles.WS_EX_TOOLWINDOW;
+            SetWindowLong(wndHelper.Handle, (int)GetWindowLongFields.GWL_EXSTYLE, (IntPtr)exStyle);
+        }
+        #region Window styles
+        [Flags]
+        public enum ExtendedWindowStyles
+        {
+            // ...
+            WS_EX_TOOLWINDOW = 0x00000080,
+            // ...
+        }
+
+        public enum GetWindowLongFields
+        {
+            // ...
+            GWL_EXSTYLE = (-20),
+            // ...
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowLong(IntPtr hWnd, int nIndex);
+
+        public static IntPtr SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong)
+        {
+            int error = 0;
+            IntPtr result = IntPtr.Zero;
+            SetLastError(0);
+
+            if (IntPtr.Size == 4)
+            {
+                // use SetWindowLong
+                Int32 tempResult = IntSetWindowLong(hWnd, nIndex, IntPtrToInt32(dwNewLong));
+                error = Marshal.GetLastWin32Error();
+                result = new IntPtr(tempResult);
+            }
+            else
+            {
+                // use SetWindowLongPtr
+                result = IntSetWindowLongPtr(hWnd, nIndex, dwNewLong);
+                error = Marshal.GetLastWin32Error();
+            }
+
+            if ((result == IntPtr.Zero) && (error != 0))
+            {
+                throw new System.ComponentModel.Win32Exception(error);
+            }
+
+            return result;
+        }
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr", SetLastError = true)]
+        private static extern IntPtr IntSetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong", SetLastError = true)]
+        private static extern Int32 IntSetWindowLong(IntPtr hWnd, int nIndex, Int32 dwNewLong);
+
+        private static int IntPtrToInt32(IntPtr intPtr)
+        {
+            return unchecked((int)intPtr.ToInt64());
+        }
+
+        [DllImport("kernel32.dll", EntryPoint = "SetLastError")]
+        public static extern void SetLastError(int dwErrorCode);
+        
+        #endregion
+
+    }
+}
+    
+
+
+
---
+++ Clock/MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+
+namespace Clock
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Clk clk;
+        double screenHeight = SystemParameters.FullPrimaryScreenHeight;
+        double screenWidth = SystemParameters.FullPrimaryScreenWidth;
+        NotifyIcon ni = new NotifyIcon();
+        public MainWindow()
+        {
+            InitializeComponent();
+            clk = new Clk();
+            clk.Show();
+            cmbColor.ItemsSource = typeof(Colors).GetProperties();
+            cmbFontSize.ItemsSource = Enumerable.Range(10, 31).ToArray();
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies;           
+            ni.Icon = new Icon("Clock.ico");
+            ni.Visible = true;
+            ni.DoubleClick +=
+                delegate (object sender, EventArgs args)
+                {
+                    this.Show();
+                    this.WindowState = WindowState.Normal;
+                    clk.txt.IsEnabled = true;
+                    clk.ccp.Background = new SolidColorBrush(Colors.Black) { Opacity = 0.01 };
+                };
+            ni.ContextMenuStrip = new ContextMenuStrip();
+            ni.ContextMenuStrip.Items.Add(new ToolStripMenuItem("Settings", null, delegate { this.Show(); clk.ccp.Background = new SolidColorBrush(Colors.Black) { Opacity = 0.01 }; clk.txt.IsEnabled = true; }));
+            ni.ContextMenuStrip.Items.Add(new ToolStripMenuItem("Exit",null,delegate { this.Close(); }));
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+                this.Hide();
+
+            base.OnStateChanged(e);
+        }
+    
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            clk.ccp.Background = (SolidColorBrush)new BrushConverter().ConvertFromString("Transparent");
+            clk.txt.IsEnabled = false;
+        }
+
+        private void cmbFontSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontSize = cmbFontSize.SelectedItem.ToString();
+            clk.FontSize = Convert.ToDouble(fontSize);
+        }
+
+        private void cmbColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            System.Windows.Media.Color selectedColor = (System.Windows.Media.Color)(cmbColor.SelectedItem as PropertyInfo).GetValue(null, null);
+            clk.Foreground = new SolidColorBrush(selectedColor);
+        }
+
+        private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            System.Windows.Media.FontFamily fontFamily = (System.Windows.Media.FontFamily)cmbFontFamily.SelectedItem;
+            clk.FontFamily = new System.Windows.Media.FontFamily(fontFamily.ToString());
+        }
+
+        private void Top_Checked(object sender, RoutedEventArgs e)
+        {
+            clk.Top = screenHeight / screenHeight;
+            clk.Left = screenWidth / 2 - clk.Width / 2;
+            clk.txt.VerticalAlignment = VerticalAlignment.Top;
+        }
+
+        private void Left_Checked(object sender, RoutedEventArgs e)
+        {
+            clk.Top = screenHeight / 2;
+            clk.Left = screenWidth / screenWidth;
+            clk.txt.VerticalAlignment = VerticalAlignment.Center;
+        }
+
+        private void Bottom_Checked(object sender, RoutedEventArgs e)
+        {
+            clk.Top = screenHeight + clk.Height / 2;
+            clk.Left = screenWidth / 2 - clk.Width / 2;
+            clk.txt.VerticalAlignment = VerticalAlignment.Bottom;
+        }
+
+        private void Right_Checked(object sender, RoutedEventArgs e)
+        {
+            clk.Top = screenHeight / 2;
+            clk.Left = screenWidth - clk.Width;
+            clk.txt.VerticalAlignment = VerticalAlignment.Center;
+        }
+
+        private void T_L_Checked(object sender, RoutedEventArgs e)
+        {
+            clk.Top = screenHeight / screenHeight;
+            clk.Left = screenWidth / screenWidth;
+            clk.txt.VerticalAlignment = VerticalAlignment.Top;
+        }
+
+        private void T_R_Checked(object sender, RoutedEventArgs e)
+        {
+            clk.Top = screenHeight / screenHeight;
+            clk.Left = screenWidth - clk.Width;
+            clk.txt.VerticalAlignment = VerticalAlignment.Top;
+        }
+
+        private void B_L_Checked(object sender, RoutedEventArgs e)
+        {
+            clk.Top = screenHeight + clk.Height / 2;
+            clk.Left = screenWidth / screenWidth;
+            clk.txt.VerticalAlignment = VerticalAlignment.Bottom;
+        }
+
+        private void B_R_Checked(object sender, RoutedEventArgs e)
+        {
+            clk.Top = screenHeight + clk.Height / 2;
+            clk.Left = screenWidth - clk.Width;
+            clk.txt.VerticalAlignment = VerticalAlignment.Bottom;
+        }
+
+        private void main_Loaded(object sender, RoutedEventArgs e)
+        {
+            clk.Owner = this;
+        }
+
+        private void Scrl_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            clk.txt.Opacity = 1 - Scrl.Value;
+        }
+
+        private void main_Closed(object sender, EventArgs e)
+        {
+            ni.Dispose();
+        }
+    }
+}
